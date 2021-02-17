f1d7cc8572777041930b5055f89268637899777b
Cyrille Rossant <rossant@users.noreply.github.com>
2021-02-17 18:49:17 +0100

    WIP: fixing volume

--- volume.py
+++ volume.py
@@ -10,9 +10,9 @@ c = canvas(show_fps=True)
 panel = c.panel(controller='arcball')
 visual = panel.visual('volume')
 
-texshape = np.array([320, 456, 528])
+texshape = np.array([456, 320, 528])
 coef = .004
-shape = coef * texshape.astype(np.float32)
+shape = coef * texshape[[1, 0, 2]]
 
 visual.data('pos', np.atleast_2d(-shape / 2), idx=0)
 visual.data('pos', np.atleast_2d(+shape / 2), idx=1)
@@ -20,12 +20,12 @@ visual.data('pos', np.atleast_2d(+shape / 2), idx=1)
 visual.data('texcoords', np.atleast_2d([0, 0, 0]), idx=0)
 visual.data('texcoords', np.atleast_2d([1, 1, 1]), idx=1)
 
-visual.data('length', np.atleast_2d(shape).astype(np.float32))
+visual.data('length', shape)
 
 # HACK
-visual.data('colormap', np.atleast_2d(np.array([0], dtype=np.float32)))
+visual.data('colormap', 5 * np.ones(1))
 
-ROOT = Path(__file__).parent.parent.parent.parent
+ROOT = Path(__file__).resolve().parent.parent / 'datoviz'
 vol = np.fromfile(ROOT / "data/volume/atlas_25.img", dtype=np.uint16)
 vol = vol.reshape(texshape)
 visual.volume(vol)
