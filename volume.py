259e1c23375f6fccb0f61a14111eb2cb497eb368
Cyrille Rossant <rossant@users.noreply.github.com>
2021-03-01 15:37:58 +0100

    Fix volume example

--- volume.py
+++ volume.py
@@ -17,17 +17,19 @@ shape = coef * texshape[[1, 0, 2]]
 visual.data('pos', np.atleast_2d(-shape / 2), idx=0)
 visual.data('pos', np.atleast_2d(+shape / 2), idx=1)
 
-visual.data('texcoords', np.atleast_2d([0, 0, 0]), idx=0)
-visual.data('texcoords', np.atleast_2d([1, 1, 1]), idx=1)
-
-visual.data('length', shape)
+visual.data('length', np.atleast_2d(shape))
 
 # HACK
-visual.data('colormap', 5 * np.ones(1))
+# visual.data('colormap', 5 * np.ones(1))
 
+# 3D texture with the volume
 ROOT = Path(__file__).resolve().parent.parent / 'datoviz'
 vol = np.fromfile(ROOT / "data/volume/atlas_25.img", dtype=np.uint16)
 vol = vol.reshape(texshape)
-visual.volume(vol)
+vol *= 10
+V = c.volume(vol)
+
+# Set the texture to the visual.
+visual.texture(V)
 
 run()
