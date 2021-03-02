9fc8503bd659864709b10ec6f9b82c6e9bcd6ff4
Cyrille Rossant <rossant@users.noreply.github.com>
2021-03-02 16:04:25 +0100

    Volume clipping

--- volume.py
+++ volume.py
@@ -17,25 +17,40 @@ shape = coef * texshape[[1, 0, 2]]
 visual.data('pos', np.atleast_2d(-shape / 2), idx=0)
 visual.data('pos', np.atleast_2d(+shape / 2), idx=1)
 
+# Box shape.
 visual.data('length', np.atleast_2d(shape))
 
-
 # Transfer function.
-fun = np.linspace(0, 1, 256).astype(np.float32)
+fun = np.linspace(0, .05, 256).astype(np.float32)
 transfer = c.transfer(fun)
 visual.texture(transfer)
+
+# X range of the transfer function
+visual.data('transferx', np.array([0, .5]))
 
 # 3D texture with the volume
 ROOT = Path(__file__).resolve().parent.parent / 'datoviz'
 vol = np.fromfile(ROOT / "data/volume/atlas_25.img", dtype=np.uint16)
 vol = vol.reshape(texshape)
-vol *= 10
+vol *= 100
 V = c.volume(vol)
 
+# GUI
 gui = c.gui("GUI")
+
+# Transfer function slider
 @gui.control("slider_float2", "transferx", vmin=0, vmax=1, force_increasing=True)
 def on_change(x0, x1):
     visual.data('transferx', np.array([x0, x1]))
+
+# Clipping
+clip = np.zeros(4, dtype=np.float32)
+clip[2] = +1
+
+@gui.control("slider_float", "clip offset", vmin=0, vmax=1, value=0)
+def on_change(x):
+    clip[3] = -x
+    visual.data('clip', clip)
 
 # Set the texture to the visual.
 visual.texture(V)
