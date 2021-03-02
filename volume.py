5d6c18d51f615801347b21546f2602ee22ea40dd
Cyrille Rossant <rossant@users.noreply.github.com>
2021-03-02 14:52:15 +0100

    Slider for transfer function in volume example

--- volume.py
+++ volume.py
@@ -19,6 +19,7 @@ visual.data('pos', np.atleast_2d(+shape / 2), idx=1)
 
 visual.data('length', np.atleast_2d(shape))
 
+
 # Transfer function.
 fun = np.linspace(0, 1, 256).astype(np.float32)
 transfer = c.transfer(fun)
@@ -30,6 +31,11 @@ vol = np.fromfile(ROOT / "data/volume/atlas_25.img", dtype=np.uint16)
 vol = vol.reshape(texshape)
 vol *= 10
 V = c.volume(vol)
+
+gui = c.gui("GUI")
+@gui.control("slider_float2", "transferx", vmin=0, vmax=1, force_increasing=True)
+def on_change(x0, x1):
+    visual.data('transferx', np.array([x0, x1]))
 
 # Set the texture to the visual.
 visual.texture(V)
