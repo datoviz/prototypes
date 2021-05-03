5466c1fdb4979f77f69d08b346f1df386f6cfdeb
Cyrille Rossant <rossant@users.noreply.github.com>
2021-05-03 13:18:30 +0200

    Add examples

---
+++ molecule.py
@@ -0,0 +1,18 @@
+"""
+# Molecule 3D mesh
+
+"""
+
+from pathlib import Path
+import numpy as np
+import numpy.random as nr
+from datoviz import canvas, run, colormap
+
+ROOT = Path(__file__).resolve().parent.parent / "datoviz"
+
+c = canvas(show_fps=False)
+panel = c.scene().panel(controller='arcball')
+visual = panel.visual('mesh')
+visual.load_obj(str(ROOT / "data/mesh/cas9_guide.obj"))
+
+run()
